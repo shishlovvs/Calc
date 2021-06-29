ac031efe04b47e81020cb090f4289114b1700b5e
shishlovvs <shishlovvs@mail.ru>
2021-06-29 19:59:44 +0400

    Add project files.

---
+++ Calc/Source.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	double a, b;
+	char s;
+	cout << "Введите арифметическое действие: ";
+	cin >> a >> s >> b;
+	cout << a << s << b << endl;
+	if (s == '+')
+		cout << a << "+" << b << "=" << a + b << endl;
+	else if (s == '-')
+		cout << a << "-" << b << "=" << a - b << endl;
+	else if (s == '*')
+		cout << a << "*" << b << "=" << a * b << endl;
+	else if (s == '/')
+		cout << a << "/" << b << "=" << a / b << endl;
+	else
+		cout << "Error: NoOperation" << endl;
+}
